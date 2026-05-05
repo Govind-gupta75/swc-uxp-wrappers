cb32eb4d71e629f9c6979601847963cc9988aa9d
gogupta <gogupta@adobe.com>
2026-05-05 19:13:14 +0530

    Upate button and button-group to 0.44.0

--- packages/icon/sp-icon.js
+++ packages/icon/sp-icon.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2026 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,18 +10,6 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { css } from 'lit';
-import { Button } from '@spectrum-web-components/button';
+import { Icon } from './src/Icon.js';
 
-class MyButton extends Button {
-    static styles = [
-        ...Button.styles,
-        css`
-            :host([class='my-button']) {
-                font-size: 20px;
-            }
-        `,
-    ];
-}
-
-customElements.define('my-button', MyButton);
+customElements.define('sp-icon', Icon);
