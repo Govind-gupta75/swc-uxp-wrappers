105ce9b38499e77d35abfae19cf8c5d3e212d2ad
gogupta <gogupta@adobe.com>
2026-05-29 14:32:56 +0530

    Fix text field typing issue

--- packages/textfield/src/Textfield.js
+++ packages/textfield/src/Textfield.js
@@ -10,10 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import {
-    TextfieldBase,
-    Textfield,
-} from '@swc-uxp-internal/textfield/src/Textfield.js';
+import { TextfieldBase } from '@swc-uxp-internal/textfield/src/Textfield.js';
 
 import styles from './uxp-textfield.css.js';
 
@@ -22,14 +19,97 @@ class UxpTextfieldBase extends TextfieldBase {
         // We are combining our styles to make all super class styles available along with the transitive dependent classes styles.
         return [...super.styles, styles];
     }
-}
 
-class UxpTextfield extends Textfield {
-    static get styles() {
-        // We are combining our styles to make all super class styles available along with the transitive dependent classes styles.
-        return [...super.styles, styles];
+    /**
+     * UXP selects/replaces input text when Lit re-syncs the `.value=${live(...)}`
+     * binding on each keystroke. Skip re-renders during active editing for fields
+     * that do not need live layout updates; restore selection for growing textareas.
+     */
+    handleInput(_event) {
+        if (this.allowedKeys && this.inputElement.value) {
+            const regExp = new RegExp(`^[${this.allowedKeys}]*$`, 'u');
+            if (!regExp.test(this.inputElement.value)) {
+                const nextSelectStart = this.inputElement.selectionStart - 1;
+                this.inputElement.value = this.value.toString();
+                this.inputElement.setSelectionRange(
+                    nextSelectStart,
+                    nextSelectStart
+                );
+                return;
+            }
+        }
+
+        const newValue = this.inputElement.value;
+        if (newValue === this.value) {
+            return;
+        }
+
+        const oldValue = this._value;
+        this._value = newValue;
+
+        if (this.focused && this.inputElement) {
+            this._uxpInputSelection = {
+                start: this.inputElement.selectionStart ?? newValue.length,
+                end: this.inputElement.selectionEnd ?? newValue.length,
+            };
+        }
+
+        if (this._uxpShouldDeferValueRender()) {
+            return;
+        }
+
+        this.requestUpdate('value', oldValue);
+    }
+
+    _uxpShouldDeferValueRender() {
+        return this.focused && !(this.multiline && this.grows);
+    }
+
+    onBlur(_event) {
+        const shouldSyncRender = this._uxpShouldDeferValueRender();
+        super.onBlur(_event);
+        this._uxpInputSelection = null;
+        if (shouldSyncRender) {
+            this.requestUpdate('value');
+        }
+    }
+
+    updated(changedProperties) {
+        super.updated(changedProperties);
+        if (
+            !this.focused ||
+            !this._uxpInputSelection ||
+            !this.inputElement
+        ) {
+            return;
+        }
+
+        const shouldRestoreSelection =
+            changedProperties.has('value') ||
+            changedProperties.has('invalid') ||
+            changedProperties.has('valid');
+
+        if (!shouldRestoreSelection) {
+            return;
+        }
+
+        const { start, end } = this._uxpInputSelection;
+        const input = this.inputElement;
+        const restoreSelection = () => {
+            const length = input.value.length;
+            input.setSelectionRange(
+                Math.min(start, length),
+                Math.min(end, length)
+            );
+        };
+
+        restoreSelection();
+        requestAnimationFrame(restoreSelection);
+        this.updateComplete.then(restoreSelection);
     }
 }
+
+class UxpTextfield extends UxpTextfieldBase {}
 
 export { UxpTextfield as Textfield };
 export { UxpTextfieldBase as TextfieldBase };
