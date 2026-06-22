b1a0fae613b580b1ee34ab0149cb156954d6f15a
gogupta <gogupta@adobe.com>
2026-06-22 13:40:22 +0530

    Upgrade switch, slider and status-light to 1.12.0

---
+++ projects/swc-starter-webpack/src/extended_swc_samples/my-slider.js
@@ -0,0 +1,27 @@
+/*
+Copyright 2026 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { Slider } from '@spectrum-web-components/slider';
+import { css } from 'lit';
+
+class MySlider extends Slider {
+    static styles = [
+        ...Slider.styles,
+        css`
+            :host {
+                --spectrum-slider-handle-border-color: coral;
+            }
+        `,
+    ];
+}
+
+customElements.define('my-slider', MySlider);
