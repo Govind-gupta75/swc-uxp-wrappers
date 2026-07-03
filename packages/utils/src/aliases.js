cf418520d246ef45f838f522568893287ba3901b
gogupta <gogupta@adobe.com>
2026-07-03 11:08:55 +0530

    Add tabs with SWC version 1.12.0

--- packages/utils/src/aliases.js
+++ packages/utils/src/aliases.js
@@ -59,6 +59,7 @@ export const aliases = {
     '@spectrum-web-components/sidenav': '@swc-uxp-wrappers/sidenav',
     '@spectrum-web-components/swatch': '@swc-uxp-wrappers/swatch',
     '@spectrum-web-components/overlay': '@swc-uxp-wrappers/overlay',
+    '@spectrum-web-components/tabs': '@swc-uxp-wrappers/tabs',
     '@spectrum-web-components/theme': '@swc-uxp-wrappers/theme',
     '@spectrum-web-components/base': '@swc-uxp-internal/base',
     '@spectrum-web-components/shared': '@swc-uxp-wrappers/shared',
