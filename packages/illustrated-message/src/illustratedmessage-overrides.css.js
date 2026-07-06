c8bc5ea2ecf7d50a495f0548de552f5c6bcead19
gogupta <gogupta@adobe.com>
2026-07-06 17:08:42 +0530

    Upgrade illustrated-message to swc 1.12.1

--- packages/illustrated-message/src/illustratedmessage-overrides.css.js
+++ packages/illustrated-message/src/illustratedmessage-overrides.css.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2026 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,14 +10,4 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { IllustratedMessage } from '@swc-uxp-internal/illustrated-message/src/IllustratedMessage.js';
-
-import styles from './uxp-illustrated-message.css.js';
-
-class UxpIllustratedMessage extends IllustratedMessage {
-    static get styles() {
-        return [...super.styles, styles];
-    }
-}
-
-export { UxpIllustratedMessage as IllustratedMessage };
+export { default } from '@swc-uxp-internal/illustrated-message/src/illustratedmessage-overrides.css.js';
