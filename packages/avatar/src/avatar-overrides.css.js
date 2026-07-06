d5289bf86740bc21cb787571b71aa1653ff82be6
gogupta <gogupta@adobe.com>
2026-07-06 17:08:08 +0530

    Upgrade avatar to swc 1.12.1

--- packages/avatar/src/avatar-overrides.css.js
+++ packages/avatar/src/avatar-overrides.css.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2026 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,8 +10,4 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-/* write uxp style overrides */
-
-/* No UXP-specific overrides needed for v1.12.0.
-   block-size / inline-size on :host and .image are translated automatically
-   by UXP's enableLogicalProperties mapper. */
+export { default } from '@swc-uxp-internal/avatar/src/avatar-overrides.css.js';
