d0c9344f567a64a6178b2f01d91aec769ee19323
gogupta <gogupta@adobe.com>
2026-07-06 17:08:49 +0530

    Upgrade menu to swc 1.12.1

--- packages/menu/src/menu-item-overrides.css.js
+++ packages/menu/src/menu-item-overrides.css.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2026 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,15 +10,5 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { MenuGroup } from '@swc-uxp-internal/menu/src/MenuGroup.js';
+export { default } from '@swc-uxp-internal/menu/src/menu-item-overrides.css.js';
 
-import styles from './uxp-menu-group.css.js';
-
-class UxpMenuGroup extends MenuGroup {
-    static get styles() {
-        // We are combining our styles to make all super class styles available along with the transitive dependent classes styles.
-        return [...super.styles, styles];
-    }
-}
-
-export { UxpMenuGroup as MenuGroup };
