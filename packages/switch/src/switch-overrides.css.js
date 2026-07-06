a801607afda3a9ccb6518d66ac1704a8d9e6f0a6
gogupta <gogupta@adobe.com>
2026-07-06 17:09:14 +0530

    Upgrade switch to swc 1.12.1

--- packages/switch/src/switch-overrides.css.js
+++ packages/switch/src/switch-overrides.css.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2026 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -10,15 +10,4 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { Switch } from '@swc-uxp-internal/switch/src/Switch.js';
-
-import styles from './uxp-switch.css.js';
-
-class UxpSwitch extends Switch {
-    static get styles() {
-        // We are combining our styles to make all super class styles available along with the transitive dependent classes styles.
-        return [...super.styles, styles];
-    }
-}
-
-export { UxpSwitch as Switch };
+export { default } from '@swc-uxp-internal/switch/src/switch-overrides.css.js';
