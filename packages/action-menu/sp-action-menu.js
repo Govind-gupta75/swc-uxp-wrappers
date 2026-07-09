1c9abd7da63ca03c193147a05400ab40a1b068e9
maxsong <maxsong@adobe.com>
2026-07-09 12:47:50 +0900

    Add Action Menu v1.12.2

---
+++ packages/action-menu/sp-action-menu.js
@@ -0,0 +1,27 @@
+/*
+Copyright 2026 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+// UXP: toggleAttribute missing on ShadyDOM wrapper objects — remove when UXP fixes ShadyDOM
+if (typeof Element !== 'undefined' && !Element.prototype.toggleAttribute) {
+    Element.prototype.toggleAttribute = function (name, force) {
+        const has = this.hasAttribute(name);
+        const on = force !== undefined ? !!force : !has;
+        if (on !== has) on ? this.setAttribute(name, '') : this.removeAttribute(name);
+        return on;
+    };
+}
+
+import { ActionMenu } from './src/ActionMenu.js';
+
+if (!customElements.get('sp-action-menu')) {
+    customElements.define('sp-action-menu', ActionMenu);
+}
